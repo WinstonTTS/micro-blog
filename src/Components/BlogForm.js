44dad2293fc2c701e5aa4ebc7f49a95b075d35d2
Blake Guilloud <blake.guilloud@gmail.com>
2018-09-20 20:32:36 -0400

    Creating new post.

--- src/Components/BlogForm.js
+++ src/Components/BlogForm.js
@@ -1,10 +1,76 @@
 import React, { Component } from 'react'
 
 class BlogForm extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      author: '',
+      title: '',
+      photo: '',
+      facebook: '',
+      twitter: '',
+      instagram: '',
+      body: '',
+    }
+  }
+
+  onChange(e) {
+    const { name, value } = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit() {
+    const payload = this.state
+    payload.publishedAt = Date.now()
+
+    fetch('http://servup.herokuapp.com/collections/blakes-blog', {
+      method: 'post',
+      body: JSON.stringify(payload),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(() => {
+      this.props.history.push('/')
+    })
+  }
+
   render() {
     return (
       <div>
-        Blog Form
+        <div>
+          <label htmlFor="author">Author</label>
+          <input onChange={this.onChange.bind(this)} name="author" type="text" />
+        </div>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input onChange={this.onChange.bind(this)} name="title" type="text" />
+        </div>
+        <div>
+          <label htmlFor="photo">Photo</label>
+          <input onChange={this.onChange.bind(this)} name="photo" type="text" />
+        </div>
+        <div>
+          <label htmlFor="twitter">Twitter</label>
+          <input onChange={this.onChange.bind(this)} name="twitter" type="text" />
+        </div>
+        <div>
+          <label htmlFor="facebook">Facebook</label>
+          <input onChange={this.onChange.bind(this)} name="facebook" type="text" />
+        </div>
+        <div>
+          <label htmlFor="instagram">Instagram</label>
+          <input onChange={this.onChange.bind(this)} name="instagram" type="text" />
+        </div>
+        <div>
+          <label htmlFor="body">Body</label>
+          <textarea onChange={this.onChange.bind(this)} name="body" id="" cols="30" rows="10"></textarea>
+        </div>
+        <button onClick={this.handleSubmit.bind(this)}>Submit Blog Post</button>
       </div>
     )
   }
--- src/Components/BlogList.js
+++ src/Components/BlogList.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+
+import './BlogList.css'
+
+class BlogItem extends Component {
+  render() {
+    const { blogPost } = this.props
+
+    return (
+      <div className="blogPost">
+        <h2>
+          {blogPost.title}
+        </h2>
+        <img src={blogPost.photo} alt=""/>
+        <div>
+          {blogPost.author}
+        </div>
+        <div>
+          Published: {moment(blogPost.publishedAt).format('MMMM Do YYYY')}
+        </div>
+        <p>
+          {blogPost.body}
+        </p>
+        <div className="socialMedia">
+          <a href={`https://www.instagram.com/${blogPost.instagram}`}>
+            <img src="https://image.flaticon.com/icons/svg/174/174855.svg" alt="" />
+          </a>
+          <a href={`https://www.facebook.com/${blogPost.facebook}`}>
+            <img src="https://image.flaticon.com/icons/svg/174/174848.svg" alt="" />
+          </a>
+          <a href={`https://www.twitter.com/${blogPost.twitter}`}>
+            <img src="https://image.flaticon.com/icons/svg/145/145812.svg" alt="" />
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
 
 class BlogList extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      blogPosts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://servup.herokuapp.com/collections/blakes-blog')
+      .then(blob => blob.json())
+      .then(data => {
+        this.setState({ blogPosts: data })
+      })
+  }
+
   render() {
     return (
       <div>
-        Blog List
+        {this.state.blogPosts.map((blogPost) => {
+          return <BlogItem key={blogPost._id} blogPost={blogPost} />
+        })}
       </div>
     )
   }
